90c775448034846622f177bb74ec166f997acc05
Christopher Miller <chris.miller0686@gmail.com>
2021-10-03 09:14:00 -0600

    my first commit

---
+++ buisness-time/buisness-time.js
@@ -0,0 +1,10 @@
+var googleButton = document.getElementById("google-button")
+
+
+function buttonClick() {
+    console.log("Button was clicked!")
+}
+
+googleButton.addEventListener("click", buttonClick)
+
+var facebookButton = document.getElementById("facebook-button")
